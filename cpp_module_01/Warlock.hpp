7f58f2ce211ba3fb21e97112425663bfa097fbef
Carmine Watcher <cwatcher@student.21-school.r>
2021-12-18 14:17:59 +0300

    [boilerplate] +cpp_module_01

---
+++ cpp_module_01/Warlock.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Warlock {
+private:
+	string	_name;
+	string	_title;
+
+	Warlock() {};
+	Warlock( Warlock const& ) {};
+	Warlock& operator=( Warlock const& ) { return *this; };
+public:
+	Warlock( string const& name, string const& title )
+	: _name( name ), _title ( title )
+	{	cout << _name << ": This looks like another boring day." << endl;
+	}
+	~Warlock()
+	{	cout<< _name << ": My job here is done!" << endl;
+	}
+	string const & getName() const { return _name; }
+	string const & getTitle() const { return _title; }
+	string& setTitle( string const& title ) { _title = title; return _title; }
+	void introduce() const
+	{	cout << _name << ": I am " << _name << ", " << _title << "!" << endl;
+	}
+};
---
+++ cpp_module_01/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include "Warlock.hpp"
+
+int	main( int ac, char* av[] )
+{
+	( void ) ac;
+	( void ) av;
+/*
+  Warlock richard("Richard", "the Titled");
+
+  Dummy bob;
+  Fwoosh* fwoosh = new Fwoosh();
+
+  richard.learnSpell(fwoosh);
+
+  richard.introduce();
+  richard.launchSpell("Fwoosh", bob);
+
+  richard.forgetSpell("Fwoosh");
+  richard.launchSpell("Fwoosh", bob);
+*/
+}
