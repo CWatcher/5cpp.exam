6b98b72cbaa84ce4d9f23aae5f367b0ac999cd92
Carmine Watcher <cwatcher@student.21-school.r>
2021-12-23 12:48:22 +0300

    [done]

--- cpp_module_00/main.cpp
+++ cpp_module_00/main.cpp
@@ -1,7 +1,13 @@
+#include "Warlock.hpp"
 
 int main()
 {
-/*
+	// Warlock bob;                            //Does not compile
+	// Warlock bob("Bob", "the magnificent");  //Compiles
+	// Warlock jim("Jim", "the nauseating");   //Compiles
+	// bob = jim;                              //Does not compile
+	// Warlock jack(jim);                      //Does not compile
+
   Warlock const richard("Richard", "Mistress of Magma");
   richard.introduce();
   std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
@@ -12,7 +18,4 @@ int main()
   jack->introduce();
 
   delete jack;
-
-  return (0);
-*/
 }
