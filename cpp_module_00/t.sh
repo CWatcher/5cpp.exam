6fed96e2b29ea9dccc37ef8b45769bc640810855
Carmine Watcher <CWatcher@student.21-school.r>
2021-12-23 08:05:33 +0300

    t.sh += echo ok!

--- cpp_module_00/t.sh
+++ cpp_module_00/t.sh
@@ -3,5 +3,4 @@
 make || exit 1
 echo
 
-# ./aa | cat -e | diff subject.out - && test [${PIPESTATUS[1]} -eq 0] && echo ok!
-./aa | cat -e | diff subject.out -
+diff subject.out <(./aa | cat -e) && echo ok!
